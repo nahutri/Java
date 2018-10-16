90ecf187fae0076d1f843b91a3cde802cd6695c1
nahutri <ngmyuni@gmail.com>
2018-10-16 08:00:50 +0200

    added Prj.java

---
+++ Prj1.java
@@ -0,0 +1,9 @@
+public class Prj1 {
+
+    public static void main (String[]args){
+        String name=JOptionPane.showInputDialog("What would you like me to do?");
+        JOptionPane.showMessageDialog(null,"I'm sorry, "+name+" I'am afraid I can't do that.");
+        System.exit(0);
+
+    }
+}

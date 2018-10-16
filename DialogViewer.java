9a0ad56506eddf04f45416e6ad0786a575d4cc2b
nahutri <ngmyuni@gmail.com>
2018-10-16 07:52:02 +0200

    added file

---
+++ DialogViewer.java
@@ -0,0 +1,13 @@
+
+public class Main {
+//15.10.2018
+    public static void main(String[] args) {
+	// write your code here
+        JOptionPane.showMessageDialog(null,"Hello, World"); //P1.9
+
+        //String name = JOptionPane.showInputDialog("What is your name");
+        //System.out.println("Hello "+name);
+        System.exit(0);
+
+    }
+}
